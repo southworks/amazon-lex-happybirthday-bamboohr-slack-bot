8d46057fad79b066238caa826a044e4581e09227
Gonzalo Muñoz <gonzalo.munoz@southworks.com>
2020-08-27 15:19:49 -0300

    config channel performance improved, channel link fixed, and works for private channels

--- birthday-bot/lambdas/slackChannel.js
+++ birthday-bot/lambdas/slackChannel.js
@@ -20,38 +20,27 @@ function dispatch(intentRequest, callback) {
 
   let response
 
-  checkChannel(channelName).then(channelNameCorrect => {
+  checkChannel(channelName).then(channel => {
 
-      if (channelNameCorrect) {
-        channels.setChannel(channelName)
-        response = `The channel #${channelName} was configured correctly`
-      }
-      else {
-        response = `The channel is not available for me, add me to the channel #${channelName}. 
-                    You can add me by clicking in "channel details", 
-                    then click on "more" and finally click on "add Apps"...`
-      }
-     
+      if (channel) {
+        channels.setChannel(channel.name);
+        response = `The channel <#${channel.id}> was configured correctly`
+      } else response = 'That channel is not available for me, add me to the channel by clicking on me, then selecting "Add this app to a channel", and finally, choosing the channel\'s name'
+  
       callback(close(sessionAttributes, 'Fulfilled',
       { 'contentType': 'PlainText', 'content': response }));
   });
 }
 
 const checkChannel = (name) => {
-  const url = `https://slack.com/api/users.conversations?token=${authToken}`
-
-  if (typeof name !== 'string' || !name || name.length <= 0) {
-    return false;
-  }
+  const url = `https://slack.com/api/users.conversations?token=${authToken}&types=public_channel,private_channel`
 
   return fetch(url, { method: 'get' })
-      .then(res => res.json())
-      .then(json => {
-
-        let channelsArray = json.channels;
-      
-        return channelsArray.length > 0 && channelsArray.filter(channel => channel.name === name).length > 0;
-      });
+    .then((res) => res.json())
+    .then((json) => {
+      const channel = json.channels.find((channel) => channel.name === name)
+      return channel ? { id: channel.id, name: channel.name } : ''
+    })
 }
 
 const config = (event, context, callback) => {
