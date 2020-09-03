4bddac59e56f1326f8a9734e318d4939dc0b3acf
Facundo Arguelles <62290512+Farguelles@users.noreply.github.com>
2020-09-03 19:46:23 -0300

    Check if the Bot is in the channel selected by the user (#16)
    * Added validation for a new channel
    * Add instructions on how to add the bot to a channel to the bot response
    * Config channel performance improved, channel link fixed, and make it works for private channels
    * Applied new logic to get Slack Auth Token

--- birthday-bot/lambdas/slackChannel.js
+++ birthday-bot/lambdas/slackChannel.js
@@ -1,4 +1,8 @@
 const channels = require('../packages/channels')
+const fetch = require('node-fetch')
+const AWS = require('aws-sdk')
+const ssm = new AWS.SSM()
+const AUTH_TOKEN_SSM = process.env.AUTH_TOKEN_SSM
 
 function close(sessionAttributes, fulfillmentState, message) {
   return {
@@ -17,22 +21,49 @@ function dispatch(intentRequest, callback) {
   const channelName = slots.channel
 
   let response
-  if (typeof channelName !== 'string' || !channelName) {
-    console.error('Validation Failed')
 
-    response = 'The channel name validation failed.'
-  } else {
-    channels.setChannel(channelName)
+  checkChannel(channelName).then((channel) => {
+    if (channel) {
+      channels.setChannel(channel.name)
+      response = `The channel <#${channel.id}> was configured correctly`
+    } else {
+      response =
+        // eslint-disable-next-line max-len
+        'That channel is not available for me, add me to the channel by clicking on me, then selecting "Add this app to a channel", and finally, choosing the channel\'s name'
+    }
 
-    response = `The channel #${channelName} was configured correctly`
+    callback(
+      close(sessionAttributes, 'Fulfilled', {
+        contentType: 'PlainText',
+        content: response,
+      })
+    )
+  })
+}
+
+const checkChannel = (name) => {
+  const ssmParams = {
+    Name: AUTH_TOKEN_SSM,
+    WithDecryption: true,
   }
 
-  callback(
-    close(sessionAttributes, 'Fulfilled', {
-      contentType: 'PlainText',
-      content: response,
+  return ssm
+    .getParameter(ssmParams, (_, data) => {})
+    .promise()
+    .then((data) => {
+      const url = 'https://slack.com/api/users.conversations?'
+      const params = new URLSearchParams({
+        token: data.Parameter.Value,
+        types: 'public_channel,private_channel',
+      })
+
+      return fetch(url + params, { method: 'get' })
+        .then((res) => res.json())
+        .then((json) => {
+          const channel = json.channels.find((channel) => channel.name === name)
+          return channel ? { id: channel.id, name: channel.name } : ''
+        })
     })
-  )
 }
 
 const config = (event, context, callback) => {
