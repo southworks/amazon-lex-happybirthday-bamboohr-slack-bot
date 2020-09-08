0fccbcc9024c64fb544c44a84ffd61ee6360c275
Facundo Arguelles <facundo.arguelles@southworks.com>
2020-09-08 12:15:20 -0300

    refactor azure.js as a class and move getBirthdaysEmails to services

--- birthday-bot/data/azure.js
+++ birthday-bot/data/azure.js
@@ -1,49 +1,44 @@
-const { getCurrentDate } = require('../helpers/utils')
 const { BlobServiceClient } = require('@azure/storage-blob')
 
-const connectionString = process.env.AZURE_BLOB_CONNECTION_STRING
-const containerName = process.env.AZURE_BLOB_CONTAINER_NAME
-const blobName = process.env.AZURE_BLOB_NAME
+class Azure {
 
-/*
- * It reads employees data from Azure Blob Store
- */
-const readStore = async () => {
-  const blobServiceClient = BlobServiceClient.fromConnectionString(
-    connectionString
-  )
-  const containerClient = blobServiceClient.getContainerClient(containerName)
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName)
+  constructor(connString, containerName, blobName) {
+    this.connectionString = connString
+    this.containerName = containerName
+    this.blobName = blobName
+  }
 
-  const downloadBlockBlobResponse = await blockBlobClient.download(0)
+  /*
+  * It reads employees data from Azure Blob Store
+  */
+  async readStore() {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+        this.connectionString
+    )
+    const containerClient = blobServiceClient.getContainerClient(this.containerName)
+    const blockBlobClient = containerClient.getBlockBlobClient(this.blobName)
 
-  const blobData = await streamToString(
-    downloadBlockBlobResponse.readableStreamBody
-  ).then((data) => JSON.parse(data))
+    const downloadBlockBlobResponse = await blockBlobClient.download(0)
 
-  return blobData
+    const blobData = await this.streamToString(
+        downloadBlockBlobResponse.readableStreamBody
+    ).then((data) => JSON.parse(data))
+
+    return blobData
+  }
+
+  /*
+  * It compose binary data to String
+  */
+  async streamToString(readableStream) {
+    const chunks = []
+
+    return new Promise((resolve, reject) => {
+        readableStream.on('data', (data) => chunks.push(data.toString()))
+        readableStream.on('end', () => resolve(chunks.join('')))
+        readableStream.on('error', reject)
+    })
+  }
 }
 
-/*
- * It compose binary data to String
- */
-const streamToString = async (readableStream) => {
-  const chunks = []
-
-  return new Promise((resolve, reject) => {
-    readableStream.on('data', (data) => chunks.push(data.toString()))
-    readableStream.on('end', () => resolve(chunks.join('')))
-    readableStream.on('error', reject)
-  })
-}
-
-const getBirthdaysEmails = async () => {
-  const today = getCurrentDate()
-  const users = await readStore()
-
-  return users
-    .filter((user) => user.Birthday.substring(5) === today)
-    .map((user) => user.Email)
-}
-
-module.exports = getBirthdaysEmails
+module.exports = Azure
--- birthday-bot/services/birthdays.js
+++ birthday-bot/services/birthdays.js
@@ -1,11 +1,21 @@
-const getBirthdaysEmails = require('../data/azure')
 const getUserIds = require('../data/slack')
 const getMsgWithEmojis = require('../packages/birthdaymessage')
+const { getCurrentDate } = require('../helpers/utils')
 
 const getBirthdaysMessage = () => {
   return getBirthdaysEmails()
     .then((emails) => getUserIds(emails))
     .then((ids) => getMsgWithEmojis(ids))
+}
+
+// WARN: This function should be refactored to use Azure class
+const getBirthdaysEmails = async () => {
+  const today = getCurrentDate()
+  const users = await readStore()
+
+  return users
+    .filter((user) => user.Birthday.substring(5) === today)
+    .map((user) => user.Email)
 }
 
 module.exports = getBirthdaysMessage
