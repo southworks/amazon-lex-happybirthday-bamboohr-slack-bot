0aeb41e9f1ba9c35471da118de9dbdec13cc1be9
Facundo Arguelles <facundo.arguelles@southworks.com>
2020-09-08 15:52:33 -0300

    add Amazon class

--- birthday-bot/data/amazon.js
+++ birthday-bot/data/amazon.js
@@ -1,55 +1,58 @@
 const AWS = require('aws-sdk')
 const S3 = new AWS.S3()
 
-const BUCKET = process.env.S3_BUCKET
-const CONFIG_KEY = 'config.json'
-const configParams = {
-  Bucket: BUCKET,
-  Key: CONFIG_KEY,
-}
+class Amazon {
 
-/* [String] Retrieve a channel name from storage S3 file */
-const getChannel = () => {
-  const params = {
-    ...configParams,
-    ResponseContentType: 'application/json',
+  BUCKET = process.env.S3_BUCKET
+  CONFIG_KEY = 'config.json'
+  configParams = {
+    Bucket: BUCKET,
+    Key: this.CONFIG_KEY,
   }
 
-  return new Promise((resolve, reject) => {
-    S3.getObject(params, (err, data) => {
-      if (err) {
-        console.log('Channel File Not Found, then I will create it')
+  constructor() {}
 
-        reject(err)
-      } else {
-        console.log(`Channel File Found: ${data.Body.toString()}`)
+  getChannel() {
+    const params = {
+      ...this.configParams,
+      ResponseContentType: 'application/json',
+    }
 
-        resolve(JSON.parse(data.Body).channel)
-      }
+    return new Promise((resolve, reject) => {
+      S3.getObject(params, (err, data) => {
+        if (err) {
+          console.log('Channel File Not Found, then I will create it')
+
+          reject(err)
+        } else {
+          console.log(`Channel File Found: ${data.Body.toString()}`)
+
+          resolve(JSON.parse(data.Body).channel)
+        }
+      })
     })
-  })
-}
-
-/* [String] Set a channel name to storage S3 file */
-const setChannel = (name) => {
-  const params = {
-    ...configParams,
-    ContentType: 'application/json',
-    Body: JSON.stringify(newChannel(name)),
   }
-  console.log(`Setting Channel: ${JSON.stringify(params)}`)
 
-  S3.putObject(params, (err, data) => {
-    if (err) console.log(err, err.stack)
-    else return data
-  })
-}
+  setChannel(name) {
+    const params = {
+      ...this.configParams,
+      ContentType: 'application/json',
+      Body: JSON.stringify(this.newChannel(name)),
+    }
+    console.log(`Setting Channel: ${JSON.stringify(params)}`)
+  
+    S3.putObject(params, (err, data) => {
+      if (err) console.log(err, err.stack)
+      else return data
+    })
+  }
 
-const newChannel = (name) => {
-  return {
-    channel: name,
-    updatedAt: new Date().toISOString(),
+  newChannel(name) {
+    return {
+      channel: name,
+      updatedAt: new Date().toISOString(),
+    }
   }
 }
 
-module.exports = { getChannel, setChannel }
+module.exports = Amazon
