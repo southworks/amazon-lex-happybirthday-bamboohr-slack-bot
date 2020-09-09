2289020c32281dc6275ea6152f23c5b9bf7b7c13
Facundo Arguelles <facundo.arguelles@southworks.com>
2020-09-09 14:23:10 -0300

    remove usage of amazon in slack class

--- birthday-bot/data/slack.js
+++ birthday-bot/data/slack.js
@@ -5,23 +5,16 @@ const AUTH_TOKEN_SSM = process.env.AUTH_TOKEN_SSM
 class Slack {
 
   lookUpByEmailURL = 'https://slack.com/api/users.lookupByEmail?'
-  
-  constructor() {
-    amazon = new Amazon()
-  }
 
-  getUserObject(userEmail) {
+  getUserObject(userEmail, token) {
     const params = new URLSearchParams({ email: userEmail })
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    }
 
-    return this.amazon.getSSMParameter(AUTH_TOKEN_SSM, true)
-      .then((token) => {
-        const headers = {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        }
-        return fetch(this.lookUpByEmailURL + params, { headers: headers })
-          .then((res) => res.json())
-      })
+    return fetch(this.lookUpByEmailURL + params, { headers: headers })
+      .then((res) => res.json())
   }
 }
 
